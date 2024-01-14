a122ceb89a32ab1bb21ecb4436f0fcb398a70558
Uthan H <uthan.hercules12@gmail.com>
2024-01-14 13:13:23 -0300

    Autocomplete key set to tab

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,4 +1,6 @@
 local lsp_zero = require('lsp-zero')
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
 
 lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
@@ -9,3 +11,8 @@ end)
 -- here you can setup the language servers 
 
 require'lspconfig'.tsserver.setup{}
+
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ['<Tab>'] = cmp.mapping.confirm({ select = true }),  })
+  })
